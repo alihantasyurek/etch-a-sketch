bb70a2b986cea86e01da89f54f280973e9a7b816
Via <alihantasyurek@gmail.com>
2025-03-26 12:31:53 +0300

    added grids

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("xd");
+let gridSize = 3;
+const container = document.querySelector("#container")
+
+function createGrids() {
+  const totalGrid = gridSize * gridSize;
+  for (let i = 0; i < totalGrid; ++i) {
+    createGrid();
+  }
+}
+
+function createGrid() {
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  grid.style.flexBasis = `${100 / gridSize}%`;
+  container.appendChild(grid);
+}
+
+createGrids();
