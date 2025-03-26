da00d8fd3484194974e0cc0726d218d3ba9a37fd
Via <alihantasyurek@gmail.com>
2025-03-26 17:34:49 +0300

    primitive version

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let gridSize = 3;
+let gridSize = 16;
 const container = document.querySelector("#container")
+const sizeBtn = document.querySelector("#size")
+const colorBtn = document.querySelector("#color");
+const resetBtn = document.querySelector("#reset")
+let isRandom = false;
 
 function createGrids() {
   const totalGrid = gridSize * gridSize;
@@ -15,4 +19,57 @@ function createGrid() {
   container.appendChild(grid);
 }
 
+function removeallGrids() {
+  const children = document.querySelectorAll(".grid")
+  children.forEach((child) => { child.remove() })
+}
+
+function generateColor() {
+  const gray = "rgb(192,192,192)";
+  const randomColor = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`;
+
+  return isRandom ? randomColor : gray;
+}
+
+function coloring() {
+  container.addEventListener("mouseover", (e) => {
+    const color = generateColor();
+    const child = e.target.closest(".grid");
+    if (child && container.contains(child)) {
+      child.style.backgroundColor = color;
+    }
+  })
+
+}
+
+sizeBtn.addEventListener("click", () => {
+  let size = prompt("Enter a number between 1 and 100");
+  const userNum = Number(size);
+  if (isNaN(userNum) || userNum > 100 || userNum <= 0) {
+    alert("Please enter a number between 1 and 100");
+    return;
+  }
+  gridSize = size;
+  removeallGrids();
+  createGrids();
+});
+
+colorBtn.addEventListener("click", () => {
+  isRandom = !isRandom;
+  if (isRandom) {
+    colorBtn.innerText = "Random: ON ";
+    colorBtn.style.backgroundColor = generateColor();
+  } else {
+    colorBtn.innerText = "Random: OFF";
+    colorBtn.style.backgroundColor = "";
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  const children = document.querySelectorAll(".grid");
+  children.forEach((child) => { child.style.backgroundColor = "" });
+});
+
 createGrids();
+coloring();
+
